731580209cfde36942cafc346711fa50fd570277
Simon Cross <hodgestar@gmail.com>
2014-07-14 12:35:38 +0200

    Setup and requirements.

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="go_contacts",
+    version="0.1.0a",
+    url='http://github.com/praekelt/go-contacts-api',
+    license='BSD',
+    description="A contacts and groups API for Vumi Go",
+    long_description=open('README.rst', 'r').read(),
+    author='Praekelt Foundation',
+    author_email='dev@praekeltfoundation.org',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'cyclone',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Internet :: WWW/HTTP',
+    ],
+)
