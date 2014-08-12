8bd1c2b53b787585157d8bbfcdb1e4d627e3a767
Rudi Giesler <rudi@praekelt.com>
2014-08-12 17:50:10 +0200

    Added basic get group

--- go_contacts/backends/groups.py
+++ go_contacts/backends/groups.py
@@ -14,6 +14,16 @@ from go_api.collections.errors import (
     CollectionObjectNotFound, CollectionUsageError)
 
 
+def group_to_dict(group):
+    """
+    Turn  a group into a dict we can return.
+    """
+    group_dict = {}
+    for key, value in group.get_data().iteritems():
+        group_dict[key] = value
+    return group_dict
+
+
 class RiakGroupsBackend(object):
     def __init__(self, riak_manager):
         self.riak_manager = riak_manager
@@ -42,3 +52,14 @@ class RiakGroupsCollection(object):
         the iterable.
         """
         raise NotImplementedError()
+
+    @inlineCallbacks
+    def get(self, object_id):
+        """
+        Return a single object from the collection. May return a deferred
+        instead of the object.
+        """
+        group = yield self.contact_store.get_group(object_id)
+        if not isinstance(group, ContactGroup):
+            raise CollectionObjectNotFound(object_id, "Group")
+        returnValue(group_to_dict(group))
--- go_contacts/backends/tests/test_groups.py
+++ go_contacts/backends/tests/test_groups.py
@@ -37,3 +37,54 @@ class TestRiakGroupsBackend(VumiTestCase):
         collection = backend.get_contact_collection("owner-1")
         self.assertEqual(collection.contact_store.user_account_key, "owner-1")
         self.assertTrue(isinstance(collection, RiakGroupsCollection))
+
+
+class TestRiakGroupsCollection(VumiTestCase):
+    def setUp(self):
+        self.persistence_helper = self.add_helper(
+            PersistenceHelper(use_riak=True, is_sync=False))
+
+    @inlineCallbacks
+    def mk_collection(self, owner_id):
+        manager = yield self.persistence_helper.get_riak_manager()
+        contact_store = ContactStore(manager, owner_id)
+        collection = RiakGroupsCollection(contact_store)
+        returnValue(collection)
+
+    EXPECTED_DATE_FORMAT = "%Y-%m-%d %H:%M:%S.%f"
+
+    GROUP_FIELD_DEFAULTS = {
+        u'name': None,
+        u'query': None,
+        u'$VERSION': None,
+    }
+
+    def assert_group(self, group, expected_partial):
+        expected = self.GROUP_FIELD_DEFAULTS.copy()
+        expected.update(expected_partial)
+        if isinstance(expected.get("created_at"), datetime):
+            expected["created_at"] = expected["created_at"].strftime(
+                self.EXPECTED_DATE_FORMAT)
+        self.assertEqual(group, expected)
+
+    @inlineCallbacks
+    def test_get(self):
+        collection = yield self.mk_collection("owner-1")
+        new_group = yield collection.contact_store.new_group(name=u"Bob")
+        group = yield collection.get(new_group.key)
+        self.assert_group(group, {
+            u'key': new_group.key,
+            u'created_at': new_group.created_at,
+            u'name': u'Bob',
+            u'user_account': u'owner-1',
+        })
+
+    @inlineCallbacks
+    def test_get_bad_key(self):
+        collection = yield self.mk_collection("owner-1")
+        error = ""
+        try:
+            group = yield collection.get('bad-key')
+        except CollectionObjectNotFound, e:
+            error = e
+        self.assertEqual(str(e), "Group 'bad-key' not found.")
