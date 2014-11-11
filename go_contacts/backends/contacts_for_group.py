67b68a584a69f437c4fe40e23ca2bb1b801962c9
Rudi Giesler <rudi@praekelt.com>
2014-11-11 12:21:42 +0200

    Added streaming for contactsforgroup

--- go_contacts/backends/contacts_for_group.py
+++ go_contacts/backends/contacts_for_group.py
@@ -14,7 +14,7 @@ from go_api.queue import PausingDeferredQueue, PausingQueueCloseMarker
 from twisted.internet.defer import maybeDeferred
 
 from .contacts import contact_to_dict
-from .utils import _get_page_of_keys
+from .utils import _get_page_of_keys, _fill_queue
 
 
 class ContactsForGroupBackend(object):
@@ -69,17 +69,65 @@ class RiakContactsForGroupCollection(object):
         self.contact_store = contact_store
         self.max_contacts_per_page = max_contacts_per_page
 
-    @inlineCallbacks
     def stream(self, group_id, query):
+        """
+        Returns a :class:`PausingDeferredQueue` of all the contacts in the
+        group. May return a deferred instead of the
+        :class:`PausingDeferredQueue`. A queue item that is an instance of
+        :class:`PausingQueueCloseMarker` indicates the end of the queue.
+
+        :param unicode group_id:
+            The ID of the group to fetch the contacts for.
+        :param unicode query:
+            Search term requested through the API. Defaults to ``None`` if no
+            search term was requested. Currently not implemented and will raise
+            a CollectionUsageError if not ``None``.
+        """
         if query is not None:
             raise CollectionUsageError("query parameter not supported")
+
         max_results = self.max_contacts_per_page
+        model_proxy = self.contact_store.contacts
+
+        def get_page(cursor):
+            return _get_page_of_keys(
+                model_proxy, group_id, max_results, cursor,
+                field_name='groups')
+
+        def get_dict(key):
+            d = self.contact_store.get_contact_by_key(key)
+            d.addCallback(contact_to_dict)
+            return d
+
         q = PausingDeferredQueue(backlog=1, size=max_results)
-        yield q.put(PausingQueueCloseMarker())
-        returnValue(q)
+        q.fill_d = _fill_queue(q, get_page, get_dict)
+        return q
 
     @inlineCallbacks
     def page(self, group_id, cursor, max_results, query):
+        """
+        Generages a page which contains a subset of the contact objects
+        belonging to a specific group.
+
+        :param unicode group_id:
+            Used to determine which group to fetch the contacts for.
+        :param unicode cursor:
+            Used to determine the start point of the page. Defaults to ``None``
+            if no cursor was supplied.
+        :param int max_results:
+            Used to limit the number of results presented in a page. Defaults
+            to ``None`` if no limit was specified.
+        :param unicode query:
+            Search term requested through the API. Defaults to ``None`` if no
+            search term was requested. Currently not implemented and will raise
+            a CollectionUsageError if not ``None``.
+
+        :return:
+            (cursor, data). ``cursor`` is an opaque string that refers to the
+            next page, and is ``None`` if this is the last page. ``data`` is a
+            list of all the objects within the page.
+        :rtype: tuple
+        """
         if query is not None:
             raise CollectionUsageError("query parameter not supported")
 
