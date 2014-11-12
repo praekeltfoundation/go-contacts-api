c7adb96ccf54a4489e24324f4533472baf398c57
Rudi Giesler <rudi@praekelt.com>
2014-11-12 16:25:13 +0200

    Added stream test for contacts for group collection

--- go_contacts/backends/tests/test_contacts_for_group.py
+++ go_contacts/backends/tests/test_contacts_for_group.py
@@ -7,10 +7,12 @@ from twisted.internet.defer import inlineCallbacks, returnValue
 from vumi.tests.helpers import VumiTestCase
 from vumi.tests.helpers import PersistenceHelper
 
+from go_api.queue import PausingQueueCloseMarker
 from go.vumitools.contact import ContactStore
 
 from go_contacts.backends.riak import (
-    ContactsForGroupBackend, RiakContactsForGroupCollection)
+    ContactsForGroupBackend, RiakContactsForGroupCollection, group_to_dict,
+    contact_to_dict)
 
 
 class TestRiakContactsForGroupBackend(VumiTestCase):
@@ -45,6 +47,56 @@ class TestRiakContactsForGroupCollection(VumiTestCase):
         returnValue(collection)
 
     @inlineCallbacks
+    def create_group(self, collection, name, query=None):
+        if query is None:
+            group = yield collection.contact_store.new_group(name)
+        else:
+            group = yield collection.contact_store.new_smart_group(name, query)
+        returnValue(group_to_dict(group))
+
+    @inlineCallbacks
+    def create_contact(self, collection, **contact_data):
+        contact = yield collection.contact_store.new_contact(**contact_data)
+        returnValue(contact_to_dict(contact))
+
+    @inlineCallbacks
+    def collect_queue(self, queue):
+        contacts = []
+        while True:
+            item = yield queue.get()
+            if isinstance(item, PausingQueueCloseMarker):
+                break
+            contacts.append(item)
+        returnValue(contacts)
+
+    @inlineCallbacks
     def test_init(self):
         collection = yield self.mk_collection("owner-1")
         self.assertEqual(collection.contact_store.user_account_key, "owner-1")
+
+    @inlineCallbacks
+    def test_stream(self):
+        collection = yield self.mk_collection("owner-1")
+        group = yield self.create_group(collection, name=u'Foo')
+        group_false = yield self.create_group(collection, name=u'Wally')
+
+        contact1 = yield self.create_contact(
+            collection, name=u'Bar', msisdn=u'+12345',
+            groups=[group.get('key')])
+        contact2 = yield self.create_contact(
+            collection, name=u'Baz', msisdn=u'+54321',
+            groups=[group.get('key')])
+        contact3 = yield self.create_contact(
+            collection, name=u'Qux', msisdn=u'+31415',
+            groups=[group.get('key'), group_false.get('key')])
+        contact4 = yield self.create_contact(
+            collection, name=u'Quux', msisdn=u'+27172',
+            groups=[group_false.get('key')])
+
+        queue = yield collection.stream(group.get('key'), None)
+        contacts = yield self.collect_queue(queue)
+
+        self.assertTrue(contact1 in contacts)
+        self.assertTrue(contact2 in contacts)
+        self.assertTrue(contact3 in contacts)
+        self.assertFalse(contact4 in contacts)
